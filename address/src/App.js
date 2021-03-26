4c168407c0d085edd045c28f04cdbba6bc86e009
awesomeusha <78401938+awesomeusha@users.noreply.github.com>
2021-03-26 12:44:42 +0530

    address in modal

--- address/src/App.js
+++ address/src/App.js
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./assets/style.css";
 import "./assets/bootstrap.min.css";
 import Address from "./components/Address";
+import AddressConfirmationDialog from "./components/AddressConfirmDialog";
 
 function App() {
   return (
     <div className="App">
       <Address />
+      <AddressConfirmationDialog />
     </div>
   );
 }
--- address/src/components/Address.js
+++ address/src/components/Address.js
@@ -1,5 +1,36 @@
 import React, { Component } from "react";
+import AddressConfirmationDialogs from "./AddressConfirmDialog";
 export default class Address extends Component {
+  state = {
+    openModal: false,
+    hno: "10",
+    society: "k-g-park",
+    landmark: "Indira bridge",
+    state: "Gujarat",
+    city: "Ahmedabad",
+    pincode: "382475",
+  };
+  cityList = [
+    { name: "Ahmedabad" },
+    { name: "Surat" },
+    { name: "Rajkot" },
+    { name: "Baroda" },
+  ];
+  stateList = [{ name: "Gujarat" }];
+  updateValues = (target) => {
+    this.setState({ [target.target.name]: target.target.value });
+  };
+
+  handleClickOpen = () => {
+    this.setState({
+      openModal: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      openModal: false,
+    });
+  };
   render() {
     return (
       <div>
@@ -14,40 +45,83 @@ export default class Address extends Component {
                       <p>
                         Address<span>*</span>
                       </p>
-                      <input name="hno" type="text" />
-                      <input name="society" type="text" />
-                      <input name="landmark" type="text" />
-                      <select name="area">
+                      <input
+                        name="hno"
+                        type="text"
+                        value={this.state.hno}
+                        onChange={this.updateValues}
+                        placeholder="Block No"
+                      />
+                      <input
+                        name="society"
+                        type="text"
+                        value={this.state.society}
+                        onChange={this.updateValues}
+                        placeholder="Society name"
+                      />
+                      <input
+                        name="landmark"
+                        type="text"
+                        value={this.state.landmark}
+                        onChange={this.updateValues}
+                        placeholder="Landmark"
+                      />
+                      <select
+                        name="city"
+                        value={this.state.city}
+                        onChange={this.updateValues}
+                      >
                         <option value="" selected>
-                          Select your delivery area
+                          Select your City
                         </option>
-                        {/* {areaList &&
-                          areaList.length > 0 &&
-                          areaList.map((data, index) => {
-                            return <option key={index}>{data.name}</option>;
-                          })} */}
+                        {this.cityList.map((data, index) => {
+                          return <option key={index}>{data.name}</option>;
+                        })}
                       </select>
                     </div>
                   </div>
                   <div className="col-lg-6 col-md-6">
                     <div className="checkout__input">
-                      <p>
-                        Town/City<span></span>
-                      </p>
-                      <input readOnly name="city" type="text" />
+                      <select
+                        name="state"
+                        onChange={this.updateValues}
+                        value={this.state.state}
+                      >
+                        <option value="" selected>
+                          Select your State
+                        </option>
+                        {this.stateList.map((data, index) => {
+                          return <option key={index}>{data.name}</option>;
+                        })}
+                      </select>
                     </div>
 
                     <div className="checkout__input">
                       <p>
                         Pin Code<span>*</span>
                       </p>
-                      <input name="pincode" type="text" />
+                      <input
+                        name="pincode"
+                        type="text"
+                        onChange={this.updateValues}
+                        value={this.state.pincode}
+                        placeholder="Pincode"
+                      />
                     </div>
                   </div>
-                  <div className="row my-button">
-                    <button type="button" className="site-btn">
+                  <div className="row">
+                    <button
+                      type="button"
+                      className="site-btn"
+                      onClick={this.handleClickOpen}
+                    >
                       Save Address
                     </button>
+                    <AddressConfirmationDialogs
+                      open={this.state.openModal}
+                      handleClose={this.handleClose}
+                      address={this.state}
+                    />
                   </div>
                 </div>
               </form>
