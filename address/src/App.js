7f30f9ac248ab6919f81b231daf2bd368f2e8f6e
awesomeusha <78401938+awesomeusha@users.noreply.github.com>
2021-03-26 18:57:55 +0530

    deleting extra files

--- address/src/App.js
+++ address/src/App.js
@@ -2,17 +2,11 @@ import logo from "./logo.svg";
 // import "./App.css";
 import "./assets/style.css";
 import "./assets/bootstrap.min.css";
-import "./assets/address.css";
-import Address from "./components/Address";
-import AddressConfirmationDialog from "./components/AddressConfirmDialog";
-import NewAddress from "./components/NewAddress";
 import CustomAddress from "./components/CustomAddress";
 
 function App() {
   return (
     <div className="App">
-      {/* <Address /> */}
-      {/* <NewAddress /> */}
       <CustomAddress />
     </div>
   );
