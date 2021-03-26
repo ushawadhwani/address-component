43319ecb4353eb95f3479f5ccd4d349165058245
techitreesoutions <Libra270177@>
2021-03-26 18:59:18 +0530

    address

--- address/src/components/CustomAddress.js
+++ address/src/components/CustomAddress.js
@@ -1,30 +1,8 @@
 import React from "react";
-const addressArray = [
-  {
-    add1: "10-a",
-    add2: "K-G Park",
-    zipcode: "382475",
-    city: "Ahmedabad",
-  },
-  {
-    add1: "11-y",
-    add2: "Swareet",
-    zipcode: "382485",
-    city: "Ahmedabad",
-  },
-  {
-    add1: "78521",
-    add2: "Green acres",
-    zipcode: "382477",
-    city: "Ahmedabad",
-  },
-  {
-    add1: "Gatlodia",
-    add2: "Karmacity",
-    zipcode: "382485",
-    city: "Ahmedabad",
-  },
-];
+import Popper from "@material-ui/core/Popper";
+import addressData from "../assets/json/address.json";
+
+const addressArray = addressData.addresses;
 
 export default class CustomAddress extends React.Component {
   constructor() {
@@ -33,13 +11,14 @@ export default class CustomAddress extends React.Component {
     this.state = {
       value: "",
       suggestions: [],
+      anchorEl: null,
     };
   }
 
   onChange = (event) => {
     var query = event.target.value;
     var newArray = addressArray.filter((item) => {
-      const suggestionText = `${item.add1} ${item.add2} ${item.city} ${item.zipcode}`;
+      const suggestionText = `${item.address1} ${item.address2} ${item.city} ${item.zipcode}`;
       if (suggestionText.toLowerCase().includes(query.toLowerCase())) {
         return item;
       }
@@ -47,6 +26,15 @@ export default class CustomAddress extends React.Component {
     this.setState({
       value: event.target.value,
       suggestions: newArray,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  listItemClicked = (event) => {
+    const data = this.state.suggestions[event.currentTarget.id];
+    this.setState({
+      value: `${data.address1}, ${data.address2}, ${data.city}, ${data.zipcode}`,
+      suggestions: [],
     });
   };
 
@@ -54,30 +42,53 @@ export default class CustomAddress extends React.Component {
     const { value, suggestions } = this.state;
 
     return (
-      <>
+      <div class="container">
         <input
           name="address"
           type="text"
           value={value}
           onChange={this.onChange}
-          placeholder="type swareet"
+          placeholder="Enter Address Here"
+          class="inputBox"
         />
+        <Popper
+          id={"simple-popper"}
+          open={value !== "" && suggestions.length > 0}
+          anchorEl={this.state.anchorEl}
+          class="paper"
+        >
+          <div class="list">
+            <b>Did you mean this address?</b>
+            <hr class="hr1" style={{ margin: "10px" }} />
+            {suggestions.map((data, index) => {
+              return (
+                <>
+                  <li onClick={this.listItemClicked} id={index}>
+                    {`${data.address1}, ${data.address2},`} <br />
+                    {`${data.city}, ${data.zipcode}`}
+                  </li>
+                  <hr class="hr" />
+                </>
+              );
+            })}
+          </div>
+        </Popper>
 
-        {value !== "" && suggestions.length > 0 && (
+        {/* {value !== "" && suggestions.length > 0 && (
           <div class="list">
             Do u mean this?
             <ul>
               {suggestions.map((data, index) => {
                 return (
                   <li key={index}>
-                    {data.add1} {data.add2} {data.zipcode} {data.city}
+                    {data.address1} {data.address2} {data.zipcode} {data.city}
                   </li>
                 );
               })}
             </ul>
           </div>
-        )}
-      </>
+        )} */}
+      </div>
     );
   }
 }
